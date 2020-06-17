631cebce64a076d49187ec6a2b3df9e57843b760
Daniel Iancu <daniel.iancu@ro.ecrion.com>
2020-06-17 15:17:50 +0300

    Add src.

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Money } from "./types";
+
+/**
+ * @param {String} str
+ * @return {String}
+ */
+function filterNumbers(str: string) {
+  return str.replace(/[^\d]/g, "");
+}
+
+/**
+ * @param {String} str
+ * @return {String}
+ */
+function filterNumbersDotsAndCommas(str: string) {
+  return str.replace(/[^\d.,]/g, "").replace(/[.,]$/, "");
+}
+
+function getDecimalSymbol(str: string) {
+  var strFiltered = filterNumbersDotsAndCommas(str);
+  var endWithZero = strFiltered[strFiltered.length - 1] === "0";
+
+  // For each character starting from the end...
+  for (var i = strFiltered.length; i > 0; i--) {
+    // If the last character is a "0" and the decimal position > 3, no decimal
+    if (strFiltered.length - i + 1 > 3 && endWithZero) {
+      return;
+    }
+
+    var currentChar = strFiltered[i - 1];
+
+    if ([",", "."].indexOf(currentChar) !== -1) {
+      return currentChar;
+    }
+  }
+}
+
+const parseMoney = (text: string) => {
+  let output: Money | null = null;
+
+  var decimalPart = "00";
+  var integerPart = "";
+  var decimalSymbol = getDecimalSymbol(text);
+
+  if (decimalSymbol) {
+    decimalPart = text.split(decimalSymbol)[1];
+    integerPart = text.split(decimalSymbol)[0];
+  } else {
+    integerPart = text;
+  }
+
+  output = {
+    amount: Number(
+      filterNumbers(integerPart) + "." + filterNumbers(decimalPart)
+    ),
+    currency: "USD",
+  };
+
+  return output;
+};
+
+export default parseMoney;
